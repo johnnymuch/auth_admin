90ef7f8c95ab5b6cd026c80e3e43682cea496c2b
khinchanny <johnny@2ntkh.com>
2017-03-29 10:51:41 +0700

    admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12 col-md-offset-0">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">User Dashboard</div>
 
                 <div class="panel-body">
                     You are logged in!
